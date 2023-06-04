3a62c2cca174f972b9ff9cc090a96ac5e3fa0b67
student_2333 <lgc2333@126.com>
2023-06-05 01:05:21 +0800

    🎉 NoneBot 2.0 🚀

--- nonebot_plugin_akinator/__init__.py
+++ nonebot_plugin_akinator/__init__.py
@@ -3,11 +3,14 @@ from nonebot.plugin import PluginMetadata
 from . import __main__ as __main__
 from .config import ConfigModel
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __plugin_meta__ = PluginMetadata(
-    "Akinator",
-    "网络天才",
-    "使用 `akinator` 指令开始让我猜人物吧！",
-    ConfigModel,
-    {"License": "MIT", "Author": "student_2333"},
+    name="Akinator",
+    description="网络天才",
+    usage="使用 `akinator` 指令开始让我猜人物吧！",
+    homepage="https://github.com/lgc-NB2Dev/nonebot-plugin-akinator",
+    type="application",
+    config=ConfigModel,
+    supported_adapters=["~onebot.v11"],
+    extra={"License": "MIT", "Author": "student_2333"},
 )
