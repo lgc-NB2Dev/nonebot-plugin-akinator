f0291d071afba96501edabca1ee9d9728be92445
student_2333 <lgc2333@126.com>
2024-04-10 22:24:22 +0800

    0.2.0.post1

--- nonebot_plugin_akinator/__init__.py
+++ nonebot_plugin_akinator/__init__.py
@@ -6,7 +6,7 @@ require("nonebot_plugin_session")
 from . import __main__ as __main__  # noqa: E402
 from .config import ConfigModel  # noqa: E402
 
-__version__ = "0.2.0"
+__version__ = "0.2.0.post1"
 __plugin_meta__ = PluginMetadata(
     name="Akinator",
     description="网络天才",
@@ -14,6 +14,9 @@ __plugin_meta__ = PluginMetadata(
     homepage="https://github.com/lgc-NB2Dev/nonebot-plugin-akinator",
     type="application",
     config=ConfigModel,
-    supported_adapters=inherit_supported_adapters("nonebot_plugin_alconna"),
+    supported_adapters=inherit_supported_adapters(
+        "nonebot_plugin_alconna",
+        "nonebot_plugin_session",
+    ),
     extra={"License": "MIT", "Author": "student_2333"},
 )
